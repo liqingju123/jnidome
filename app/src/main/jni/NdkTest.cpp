22313d65fa1a10eb28e3b2b3f59d3ed32a122e43
imac <1>
2016-10-13 15:24:14 +0800

    添加打印

--- app/src/main/jni/NdkTest.cpp
+++ app/src/main/jni/NdkTest.cpp
@@ -1,6 +1,11 @@
-
-
+#include <jni.h>
 #include "com.li.qingju.jnidemo.NdkTest.h"
+#include <string.h>
+#include <android/log.h>
+#define   LOG_TAG    "LOG_TEST"
+#define   LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define   LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
 
 JNIEXPORT jstring JNICALL Java_com_li_qingju_jni_demo_NdkTest_getString(JNIEnv *env, jclass type) {
 
@@ -9,6 +14,6 @@ JNIEXPORT jstring JNICALL Java_com_li_qingju_jni_demo_NdkTest_getString(JNIEnv *
 
 
 JNIEXPORT jint  JNICALL Java_com_li_qingju_jnidemo_NdkTest_doAdd(JNIEnv *env, jclass type, jint param1, jint param2) {
-
-    return param1 + param1;
+    LOGI("我只是打印而已");
+    return param1 + param2;
 }
